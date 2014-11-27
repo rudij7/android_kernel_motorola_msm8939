5b8ffc8a63845dcf3e790b41bd2f499ef6276398
Ravi Vembu <raviv@motorola.com>
2014-11-26 18:26:18 -0600

    usb: phy: Fix broken charger detection
    The commit 691f39770bf7ed8a36a35dfb7a0eb74138767034 causes the
    chg_state to become USB_CHG_STATE_DETECTED when there is no
    charger connected. During subsequent connections, the msm_chg_detect_work
    does not run and charger type defaults to SDP. Fix this by defaulting
    only the value reported to userspace to POWER_SUPPLY_TYPE_USB.
    
    Change-Id: I9e8158d582a07d97c445aafbede028f766f1a5f5
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/693386
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -1878,7 +1878,7 @@ static int msm_otg_notify_chg_type(struct msm_otg *motg)
 		motg->chg_type == USB_ACA_C_CHARGER))
 		charger_type = POWER_SUPPLY_TYPE_USB_ACA;
 	else
-		charger_type = POWER_SUPPLY_TYPE_USB;
+		charger_type = POWER_SUPPLY_TYPE_UNKNOWN;
 
 	if (!psy) {
 		pr_err("No USB power supply registered!\n");
@@ -4799,6 +4799,7 @@ static int otg_power_set_property_usb(struct power_supply *psy,
 			break;
 		default:
 			motg->chg_type = USB_INVALID_CHARGER;
+			psy->type = POWER_SUPPLY_TYPE_USB;
 			break;
 		}
 
