MotorolaMobilityLLC/kernel-msm

msm: mdss: Parsing panel info from LK
Parsing the panel information which are passing through the kernel
boot argument or the DTS

Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>

drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,6 +26,8 @@
 
 #include "mdss_dsi.h"
 
+#define MDSS_PANEL_DEFAULT_VER 0xffffffffffffffff
+#define MDSS_PANEL_UNKNOWN_NAME "unknown"
 #define DT_CMD_HDR 6
 
 #define DCS_CMD_GET_POWER_MODE 0x0A    /* get power_mode */
@@ -387,6 +389,56 @@ static int mdss_dsi_roi_merge(struct mdss_dsi_ctrl_pdata *ctrl,
 	}
 
 	return ans;
+}
+
+int mdss_panel_parse_panel_config_dt(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
+{
+	struct device_node *np;
+	const char *pname;
+	u32 panel_ver;
+
+	np = of_find_node_by_path("/chosen");
+	ctrl_pdata->panel_config.esd_disable_bl =
+			of_property_read_bool(np, "mmi,esd");
+	if (ctrl_pdata->panel_config.esd_disable_bl)
+		pr_warn("%s: ESD detection is disabled by UTAGS\n", __func__);
+
+	if (of_property_read_bool(np, "mmi,bare_board") == true)
+		ctrl_pdata->panel_config.bare_board = true;
+
+	ctrl_pdata->panel_config.panel_ver = MDSS_PANEL_DEFAULT_VER;
+	of_property_read_u64(np, "mmi,panel_ver",
+					&ctrl_pdata->panel_config.panel_ver);
+
+	pname = of_get_property(np, "mmi,panel_name", NULL);
+	if (!pname || strlen(pname) == 0) {
+		pr_warn("Failed to get mmi,panel_name\n");
+		strlcpy(ctrl_pdata->panel_config.panel_name,
+				MDSS_PANEL_UNKNOWN_NAME,
+				sizeof(ctrl_pdata->panel_config.panel_name));
+	} else
+		strlcpy(ctrl_pdata->panel_config.panel_name, pname,
+				sizeof(ctrl_pdata->panel_config.panel_name));
+
+	pr_debug("%s: esd_enable=%d bare_board_bl=%d factory_cable=%d "
+				"panel_name=%s\n",
+				__func__, ctrl_pdata->panel_config.esd_enable,
+				ctrl_pdata->panel_config.bare_board,
+				of_property_read_bool(np, "mmi,factory-cable"),
+				ctrl_pdata->panel_config.panel_name);
+
+	panel_ver = (u32)ctrl_pdata->panel_config.panel_ver;
+	pr_info("BL: panel=%s, manufacture_id(0xDA)= 0x%x "
+		"controller_ver(0xDB)= 0x%x controller_drv_ver(0XDC)= 0x%x, "
+		"full=0x%016llx\n",
+		ctrl_pdata->panel_config.panel_name,
+		panel_ver & 0xff, (panel_ver & 0xff00) >> 8,
+		(panel_ver & 0xff0000) >> 16,
+		ctrl_pdata->panel_config.panel_ver);
+
+	of_node_put(np);
+
+	return 0;
 }
 
 static char caset[] = {0x2a, 0x00, 0x00, 0x03, 0x00};	/* DTYPE_DCS_LWRITE */

rudij7/android_kernel_motorola_msm8939

Port of 2868804 and a small part of c459577.
Change-Id: Ia4041fcbd59175b4dae891f332e8510735e45c3d
Signed-off-by: Zac Crosby <zac@motorola.com>
Reviewed-on: http://gerrit.mot.com/653176
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
Reviewed-by: Shi-Yong Li <a22381@motorola.com>
Reviewed-by: Joseph Swantek <jswantek@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,8 @@
 
 #include "mdss_dsi.h"
 
+#define MDSS_PANEL_DEFAULT_VER 0xffffffffffffffff
+#define MDSS_PANEL_UNKNOWN_NAME "unknown"
 #define DT_CMD_HDR 6
 
 /* NT35596 panel specific status variables */
@@ -401,6 +403,56 @@ static int mdss_dsi_roi_merge(struct mdss_dsi_ctrl_pdata *ctrl,
 	}
 
 	return ans;
+}
+
+int mdss_panel_parse_panel_config_dt(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
+{
+	struct device_node *np;
+	const char *pname;
+	u32 panel_ver;
+
+	np = of_find_node_by_path("/chosen");
+	ctrl_pdata->panel_config.esd_enable =
+		!of_property_read_bool(np, "mmi,esd");
+	if (!ctrl_pdata->panel_config.esd_enable)
+		pr_warn("%s: ESD detection is disabled by UTAGS\n", __func__);
+
+	if (of_property_read_bool(np, "mmi,bare_board") == true)
+		ctrl_pdata->panel_config.bare_board = true;
+
+	ctrl_pdata->panel_config.panel_ver = MDSS_PANEL_DEFAULT_VER;
+	of_property_read_u64(np, "mmi,panel_ver",
+					&ctrl_pdata->panel_config.panel_ver);
+
+	pname = of_get_property(np, "mmi,panel_name", NULL);
+	if (!pname || strlen(pname) == 0) {
+		pr_warn("Failed to get mmi,panel_name\n");
+		strlcpy(ctrl_pdata->panel_config.panel_name,
+				MDSS_PANEL_UNKNOWN_NAME,
+				sizeof(ctrl_pdata->panel_config.panel_name));
+	} else
+		strlcpy(ctrl_pdata->panel_config.panel_name, pname,
+				sizeof(ctrl_pdata->panel_config.panel_name));
+
+	pr_debug("%s: esd_enable=%d bare_board_bl=%d factory_cable=%d "
+				"panel_name=%s\n",
+				__func__, ctrl_pdata->panel_config.esd_enable,
+				ctrl_pdata->panel_config.bare_board,
+				of_property_read_bool(np, "mmi,factory-cable"),
+				ctrl_pdata->panel_config.panel_name);
+
+	panel_ver = (u32)ctrl_pdata->panel_config.panel_ver;
+	pr_info("BL: panel=%s, manufacture_id(0xDA)= 0x%x "
+		"controller_ver(0xDB)= 0x%x controller_drv_ver(0XDC)= 0x%x, "
+		"full=0x%016llx\n",
+		ctrl_pdata->panel_config.panel_name,
+		panel_ver & 0xff, (panel_ver & 0xff00) >> 8,
+		(panel_ver & 0xff0000) >> 16,
+		ctrl_pdata->panel_config.panel_ver);
+
+	of_node_put(np);
+
+	return 0;
 }
 
 static char caset[] = {0x2a, 0x00, 0x00, 0x03, 0x00};	/* DTYPE_DCS_LWRITE */